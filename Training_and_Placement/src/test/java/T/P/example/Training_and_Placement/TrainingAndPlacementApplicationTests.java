204ee55ab307425a890efd9b6d789a17a023c8ea
Ankita Vairagade <ankita.wairagadepauni@gmail.com>
2026-06-11 11:28:43 +0530

    Initial Spring Boot Project

---
+++ Training_and_Placement/src/test/java/T/P/example/Training_and_Placement/TrainingAndPlacementApplicationTests.java
@@ -0,0 +1,13 @@
+package T.P.example.Training_and_Placement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAndPlacementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
